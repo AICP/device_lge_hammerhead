351b460e0201a3e73f8faa8bd6ce9cca93b18076
LorDClockaN <davor@losinj.com>
2014-11-09 08:45:04 +0100

    Proper blobs locations and manipulation
    Change-Id: I923e99cb221d83f0eddc04fd21ea4ec4d9f9dad6

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -30,5 +30,6 @@ PRODUCT_MODEL := AOSP on HammerHead
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/hammerhead/device.mk)
-$(call inherit-product-if-exists, vendor/lge/hammerhead/hammerhead-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/hammerhead/device-vendor.mk)
